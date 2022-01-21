bc4c46ed8fb9d120614f289cc6d773408777d80a
mikolaj6r <mikolaj6r@gmail.com>
2022-01-22 00:00:09 +0100

    chore: add mocks

---
+++ src/mocks/handlers.js
@@ -0,0 +1,83 @@
+// src/mocks/handlers.js
+import { rest } from "msw";
+import exampleResponse from "./ratesResponse.json";
+
+function moneyConverter(amount, baseRateInUSD, destRateInUSD) {
+  const convertedPrice = amount * (1 / baseRateInUSD) * destRateInUSD;
+  return convertedPrice;
+}
+
+async function fetchRatesOnline(ctx) {
+  const response = await ctx.fetch(
+    `https://openexchangerates.org/api/latest.json?app_id=${process.env.REACT_APP_API_ID}`
+  );
+  const responseData = await response.json();
+  return responseData;
+}
+
+function fetchRatesMock() {
+  return exampleResponse;
+}
+
+function fetchRates(ctx) {
+  if (process.env.NODE_ENV === "test") return fetchRatesMock();
+  else return fetchRatesOnline(ctx);
+}
+
+export const handlers = [
+  rest.get("/api/latest", async (req, res, ctx) => {
+    // Perform an original request to the intercepted request URL
+    const originalResponseData = await fetchRates(ctx);
+
+    const base = req.url.searchParams.get("base") || "USD";
+    const { rates, isMockData = false } = originalResponseData;
+    const baseRate = rates[base];
+
+    if (!baseRate) return res(ctx.json({ error: "Wrong BASE provided." }));
+
+    const newRates = Object.entries(rates).reduce((acc, entry) => {
+      const [currency, rate] = entry;
+      const convertedRate = moneyConverter(1, baseRate, rate);
+
+      acc[currency] = convertedRate;
+
+      return acc;
+    }, {});
+
+    return res(
+      ctx.json({ base, isMockData, rates: newRates, timestamp: Date.now()  })
+    );
+  }),
+
+  rest.get("/api/convert/:value/:from/:to", async (req, res, ctx) => {
+    const originalResponseData = await fetchRates(ctx);
+
+    const { value, from, to } = req.params;
+    const { rates, isMockData = false } = originalResponseData;
+    const fromRate = rates[from];
+    const toRate = rates[to];
+
+    if (!fromRate || !toRate || !value)
+      return res(ctx.json({ error: "Too few params provided." }));
+
+    const rate = moneyConverter(1, fromRate, toRate);
+    const convertedAmount = moneyConverter(value, fromRate, toRate);
+
+    return res(
+      ctx.json({
+        isMockData,
+        request: {
+          query: req.url.pathname,
+          amount: value,
+          from,
+          to,
+        },
+        meta: {
+          timestamp: Date.now(),
+          rate,
+        },
+        response: convertedAmount,
+      })
+    );
+  }),
+];
